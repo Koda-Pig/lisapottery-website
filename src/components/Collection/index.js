3139a4cf359305a4d31db2080e5968ec6242a719
Josh Koter <20446632+Koda-Pig@users.noreply.github.com>
2024-05-25 18:04:17 +0200

    image loader

--- src/components/Collection/index.js
+++ src/components/Collection/index.js
@@ -8,10 +8,28 @@ export const Collection = ({ props }) => {
   const images = props.images;
   const [selected, setSelected] = useState(null);
   const [galleryOpen, setGalleryOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (selected !== null) setGalleryOpen(true);
   }, [selected]);
+
+  useEffect(() => {
+    const handleLoading = () => setLoading(false);
+    if (images.length > 0) {
+      setLoading(true);
+      Promise.all(
+        images.map(
+          (image) =>
+            new Promise((resolve) => {
+              const img = new Image();
+              img.src = image.src;
+              img.onload = resolve;
+            })
+        )
+      ).then(handleLoading);
+    }
+  }, [images]);
 
   const handlePrevious = () => {
     if (selected === null) return;
@@ -38,6 +56,28 @@ export const Collection = ({ props }) => {
         selected !== null ? styles.has_selected : ""
       }`}
     >
+      {loading && (
+        <div className={styles.loader}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={36}
+            height={36}
+            viewBox="0 0 24 24"
+          >
+            <style>
+              {"@keyframes spinner_jgYN{to{transform:rotate(360deg)}}"}
+            </style>
+            <path
+              d="M2 12A11.2 11.2 0 0 1 13 1.05C12.67 1 12.34 1 12 1a11 11 0 0 0 0 22c.34 0 .67 0 1-.05C6 23 2 17.74 2 12Z"
+              style={{
+                transformOrigin: "center",
+                animation: "spinner_jgYN .6s linear infinite"
+              }}
+            />
+          </svg>
+        </div>
+      )}
+
       {/* buttons */}
       <button
         type="button"
