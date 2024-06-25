bad754ca1409aa0203f6eaf99a0e41d604192faa
Josh Koter <20446632+Koda-Pig@users.noreply.github.com>
2024-06-25 19:13:46 +0200

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "../styles/globals.scss";
 
 export const metadata = {
@@ -12,6 +13,21 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" id="top">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NR56J0G51G"
+      ></Script>
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NR56J0G51G');
+          `
+        }}
+      />
       <body>{children}</body>
     </html>
   );
